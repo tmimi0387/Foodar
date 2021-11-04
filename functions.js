8546eed535fcb7be6f76fd2970e3624aa929dd0f
tmimi0387 <66104801+tmimi0387@users.noreply.github.com>
2021-11-04 15:04:31 -0400

    fixed javascript
    Fixed the JavaScript that automatically calculates the total. Also, made radio button bubble selectors bigger.

--- functions.js
+++ functions.js
@@ -7,24 +7,3 @@ function checkZip() {
     }
 }
 
-function total() {
-    document.getElementByID("classic").value = 3; document.getElementByID("premium").value = 5; document.getElementByID("royal").value = 7;
-    document.getElementById("_10").value = 0.10; document.getElementByID("_15").value = 0.15; document.getElementByID("_20").value = 0.2;
-
-    var price = document.getElementsByClassName("style").value ;
-    var tip = document.getElementsByClassName("tip").value;
-    for( i = 0; i < price.length; i++ ) {
-        if(price[i].type = "radio" && price[i].checked) {
-            document.getElementsByClassName("style")
-        }
-    }
-    
-    var x = price * tip;
-    var cost = price + x;
-    document.getElementsByID("total").innerhtml = cost;
-}
-
-/* 
-if(ele[i].checked)
-    document.getElementById("result").innerHTML += ele[i].name + " Value: " + ele[i].value + "<br>";
-*/
